b0172f552c518dffdedeb155cb86aa03006526c4
NewAffableGenes <160347666+NewAffableGenes@users.noreply.github.com>
2024-04-03 20:06:24 +0100

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -5,12 +5,12 @@ $server = filter_input(INPUT_SERVER, 'SERVER_NAME');
 if (($server == 'localhost') || ($server == 'www.affablegenes.com')){
     define("HOST", "localhost");     // The host you want to connect to.
     define("USER", "affableg");    // The database username.
-    define("PASSWORD", "R1lstone!");    // The database password.
+    define("PASSWORD", "");    // The database password.
     define("DATABASE", "affableg");    // The database name.
 } else if ($server == '35.177.231.21') {
     define("HOST", "localhost");     // The host you want to connect to.
     define("USER", "affableg");    // The database username.
-    define("PASSWORD", "R1lstone!");    // The database password.
+    define("PASSWORD", "");    // The database password.
     define("DATABASE", "affableg");    // The database name.
 } else {
     echo "Invalid Server = " . $server . "<br>";
